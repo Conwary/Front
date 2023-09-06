a20e43c8253eb9c88271365634545a594a6e3ca3
Conwary <guja26082546@gmail.com>
2023-09-06 12:33:13 +0700

    M

--- pages/AddMember.js
+++ pages/AddMember.js
@@ -2,7 +2,7 @@ import { useSession, signIn, signOut } from "next-auth/react";
 import { useRouter } from "next/router";
 
 export async function getServerSideProps() {
-  const res = await fetch('http://localhost:3000/api/users');
+  const res = await fetch('http://localhost:3001/api/users');
   const data = await res.json();
   const posts = data.users || []; // กำหนด posts เป็นอาเรย์ว่างหาก data.users เป็น undefined
 

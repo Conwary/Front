b7bb3c9458be9cec420666637a47732a9dffc79e
Conwary <guja26082546@gmail.com>
2023-09-06 18:33:38 +0700

    Ken

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,11 @@ import Link from 'next/link';
 import { useRouter } from "next/router";
 import 'bootstrap/dist/css/bootstrap.min.css';
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous"></link>
-export default function Home() {
+function Home() {
+  const router = useRouter();
   return (
       <>
+      
 <Head>
       <title>MassShop</title>
 </Head>
@@ -100,7 +102,7 @@ export default function Home() {
       </>
   );
 }
-  
+export default Home;
 
 
 

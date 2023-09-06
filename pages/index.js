8f17caf8f1b949bc8c7f7a2ad9c6c131ddc93107
Conwary <guja26082546@gmail.com>
2023-09-06 18:25:25 +0700

    Max

--- pages/index.js
+++ pages/index.js
@@ -11,27 +11,6 @@ export default function Home() {
 </Head>
 
 <main className="text-center data-bs-toggle mb-4">
-  <div id="carouselExample " className="carousel slide data-bs-toggle">
-    <div className="carousel-inner data-bs-toggle">
-      <div className="carousel-item active">
-        <img src="https://media.discordapp.net/attachments/424843708396077057/1115824952491380737/photo-1553095066-5014bc7b7f2d.png?width=1017&height=677" className="d-block w-100" alt="1" />
-      </div>
-      <div className="carousel-item data-bs-toggle">
-        <img src="https://media.discordapp.net/attachments/622433942766878742/1113178801174745218/image.png?width=246&height=245" className="d-block w-100" alt="2" />
-      </div>
-      <div className="carousel-item">
-        <img src="https://media.discordapp.net/attachments/622433942766878742/1113058213605154826/350352705_1173658946639499_2669946529075932660_n.png?width=381&height=677" className="d-block w-100" alt="3" />
-      </div>
-    </div>
-    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-      <span className="carousel-control-prev-icon" aria-hidden="true" />
-      <span className="visually-hidden">Previous</span>
-    </button>
-    <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-      <span className="carousel-control-next-icon" aria-hidden="true" />
-      <span className="visually-hidden">Next</span>
-    </button>
-  </div>
   <div className="row text-dark mt-4">
       <div class="col-lg-4">
       <img width="140" height="140" src="https://media.discordapp.net/attachments/983433055924527135/983553909417185391/caption-1.gif" className="bd-placeholder-img rounded-circle" alt="1" />
